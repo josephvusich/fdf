8e4cd4881f552fa0b93ca69af96ca3b67098b234
Joseph Vusich <15684831+josephvusich@users.noreply.github.com>
2021-08-03 21:45:09 +0000

    test: add no_clone build tag to skip tests involving cloning on unsupported filesystems

--- scanner_test.go
+++ scanner_test.go
@@ -130,36 +130,6 @@ func TestScanner_LinkUnlink(t *testing.T) {
 	})
 }
 
-func TestScanner_Clone(t *testing.T) {
-	assert := require.New(t)
-	setupTest(assert, func(l *testLayout, validate func(*testLayout)) {
-		scanner := newScanner()
-		scanner.options.clone = true
-		scanner.options.Recursive = true
-		scanner.options.MatchMode = matchContent
-
-		assert.NoError(scanner.Scan())
-		fmt.Println(scanner.totals.PrettyFormat(scanner.options.Verb()))
-		assert.Equal(uint64(22), scanner.totals.Files.count)
-		assert.Equal(uint64(73), scanner.totals.Files.size)
-		assert.Equal(uint64(7), scanner.totals.Unique.count)
-		assert.Equal(uint64(33), scanner.totals.Unique.size)
-		assert.Equal(uint64(0), scanner.totals.Links.count)
-		assert.Equal(uint64(0), scanner.totals.Links.size)
-		assert.Equal(uint64(15), scanner.totals.Cloned.count)
-		assert.Equal(uint64(40), scanner.totals.Cloned.size)
-		assert.Equal(uint64(0), scanner.totals.Dupes.count)
-		assert.Equal(uint64(0), scanner.totals.Dupes.size)
-		assert.Equal(uint64(15), scanner.totals.Processed.count)
-		assert.Equal(uint64(40), scanner.totals.Processed.size)
-		assert.Equal(uint64(0), scanner.totals.Skipped.count)
-		assert.Equal(uint64(0), scanner.totals.Skipped.size)
-		assert.Equal(uint64(0), scanner.totals.Errors.count)
-		assert.Equal(uint64(0), scanner.totals.Errors.size)
-		validate(l)
-	})
-}
-
 func TestScanner_Delete(t *testing.T) {
 	assert := require.New(t)
 	setupTest(assert, func(l *testLayout, validate func(*testLayout)) {
