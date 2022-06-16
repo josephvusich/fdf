757f8cd1960f1ff45d6b05086d05b38386f74ba3
Joseph Vusich <15684831+josephvusich@users.noreply.github.com>
2022-06-16 00:47:19 +0000

    Revert "Expand patterns matched by copyname to include 'xxy.z' ~= 'xx.y.z' ~= 'xx.z'"
    This reverts commit e0a80362fdd310e7856e405bd14058d51c855224.

--- copyname.go
+++ copyname.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"path/filepath"
 	"regexp"
 	"strings"
 )
@@ -11,38 +10,9 @@ var copyNamePattern = regexp.MustCompile(`^(?i)\s*(?:Copy (?:\d+ )?of )?((?U).*)
 
 // Note that isCopyName must be transitive or links can be created and broken in a single traversal
 func isCopyName(nameA, nameB string) bool {
-	if len(nameA) == 0 || len(nameB) == 0 {
-		return false
-	}
-
 	names := []string{
 		nameA,
 		nameB,
-	}
-
-	parts := make([][]string, 2)
-	shortestBase := -1
-	for i, name := range names {
-		lastExt := ""
-		for partExt := filepath.Ext(name); partExt != ""; partExt = filepath.Ext(name) {
-			if lastExt == "" {
-				lastExt = partExt
-			}
-			name = strings.TrimSuffix(name, partExt)
-		}
-
-		parts[i] = []string{
-			name,
-			lastExt,
-		}
-
-		if l := len(name); shortestBase == -1 || l < shortestBase {
-			shortestBase = l
-		}
-	}
-
-	if shortestBase != 0 && parts[0][0][0:shortestBase] == parts[1][0][0:shortestBase] && parts[0][1] == parts[1][1] {
-		return true
 	}
 
 	for i := range names {
