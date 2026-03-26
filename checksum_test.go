430605b59fa18d66b0df86c46c3baabae8680b05
Joseph Vusich <15684831+josephvusich@users.noreply.github.com>
2026-03-26 18:26:44 +0000

    test: add unit tests for hwhChecksum (determinism, collision, size)

--- checksum_test.go
+++ checksum_test.go
@@ -9,6 +9,45 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestHwhChecksum_Deterministic(t *testing.T) {
+	assert := require.New(t)
+	data := []byte("deterministic input data")
+
+	h1, err := hwhChecksum(bytes.NewReader(data))
+	assert.NoError(err)
+	h2, err := hwhChecksum(bytes.NewReader(data))
+	assert.NoError(err)
+
+	assert.Equal(h1, h2)
+}
+
+func TestHwhChecksum_DifferentInputs(t *testing.T) {
+	assert := require.New(t)
+
+	h1, err := hwhChecksum(bytes.NewReader([]byte("input A")))
+	assert.NoError(err)
+	h2, err := hwhChecksum(bytes.NewReader([]byte("input B")))
+	assert.NoError(err)
+
+	assert.NotEqual(h1, h2)
+}
+
+func TestHwhChecksum_EmptyInput(t *testing.T) {
+	assert := require.New(t)
+
+	h, err := hwhChecksum(bytes.NewReader(nil))
+	assert.NoError(err)
+	assert.Len(h, ChecksumBlockSize)
+}
+
+func TestHwhChecksum_ResultSize(t *testing.T) {
+	assert := require.New(t)
+
+	h, err := hwhChecksum(bytes.NewReader([]byte("some data")))
+	assert.NoError(err)
+	assert.Len(h, ChecksumBlockSize)
+}
+
 func BenchmarkChecksum_hwhContinuous(b *testing.B) {
 	assert := require.New(b)
 	buf := make([]byte, 2048*2048*100)
